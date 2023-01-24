8dda973f7f9ebc9dd1431c664aca3d1907e0fcf5
mosaraf <you@example.com>
2023-01-24 19:25:22 +0530

    This is me

---
+++ run.py
@@ -0,0 +1,8 @@
+def runrun():
+    print("docker build -t biodata .")
+    print("docker run -it -p 8082:8000 biodata")
+
+
+runrun()
+    
+
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t biodata .
-sudo docker run -it -p 8082:8000 biodata
